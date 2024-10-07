6d78c1dac3e9d3e398676b956346ef6c9de2d9f0
incognito1025 <nkhanamart@gmail.com>
2024-10-06 21:18:17 -0400

    Updated RegistrationForm.jsx path to import RegistrationForm from '../Components/RegistrationForm/RegistrationForm';

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './Login.css'; // Import the CSS file for styling
-import RegistrationForm from './RegistrationForm'; // Import the RegistrationForm component
+import RegistrationForm from '../Components/RegistrationForm/RegistrationForm'; // Import the RegistrationForm component
 import { useNavigate } from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'; // Import hamburger icon
 
