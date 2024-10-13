efe18ea243cfa55d4b02ca20b8dcfe84f271a270
ayoublos <abelibel@pursuit.org>
2024-10-13 10:29:36 -0400

    saving before pulling anthonys push for login

--- src/Components/Dish/dish.jsx
+++ src/Components/Dish/dish.jsx
@@ -1,32 +1,49 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-export default function Dish({item,index}) {
-  
-  function ratingDishes(number){
-    let string=' '
-    for(let i=0;i<number;i++){
-      string+='🍽️ '
+export default function Dish({ item, index }) {
+  function ratingDishes(number) {
+    let string = " ";
+    for (let i = 0; i < number; i++) {
+      string += "🍽️ ";
     }
-    return string
-
+    return string;
   }
-    let navigate = useNavigate()
+  let navigate = useNavigate();
 
   return (
-    <div onClick={()=>navigate(`/dishes/${item.dish_id}`)}  className="nearbyoptions-container" key={index}>
-    <div className="nearbyoptions_item">
-    <img className="nearbyoptions_item_image" src={item.dish_image} alt="" />
-      <div className="nearbyoptions_item_name&restaurant">
-        <p className="nearbyoptions_item_dish-name_title">Dish Name:</p>
-      <h5 className="nearbyoptions_item_dish-name" >{item.dish_name}</h5>
-      <p className="nearbyoptions_item_restaurant-name_title">Restaurant Name:</p>
-      <p className="nearbyoptions_item_restaurant-name">{item.restaurant_name}</p>
-      <p className="nearbyoptions_item_rating_title">Rating:</p>
-      <h3 className='nearbyoptions_rating-content'>{item.avg_rating}</h3>
+    <div
+      onClick={() => navigate(`/dishes/${item.dish_id}`)}
+      className="nearbyoptions-container"
+      key={index}
+    >
+      <div className="nearbyoptions_item">
+        <img
+          className="nearbyoptions_item_image"
+          src={item.dish_image}
+          alt=""
+        />
+        <div className="nearbyoptions_item_name&restaurant">
+          <div className="nearbyoptions_item_dish-restaurant-container">
+          <img className="nearbyoptions_icon"  src="/restaurant.svg" />
+          <p className="nearbyoptions_item_restaurant-name">
+            {item.restaurant_name}
+          </p>
 
+          </div>
+          <div className="nearbyoptions_item_dish-name-container">
+            <img className="nearbyoptions_icon" src="/dinner.svg" />
+
+            <h5 className="nearbyoptions_item_dish-name">{item.dish_name}</h5>
+          </div>
+          <div className="nearbyoptions_item_dish-rating-container">
+
+            <img className="nearbyoptions_icon"  src="/dish.png" />
+            <h3  className="nearbyoptions_rating-content">{item.avg_rating}</h3>
+            <img className="nearbyoptions_icon"  src="distance.png" />
+            <h3  className="nearbyoptions_distance-content">{item.avg_rating}</h3>
+          </div>
+        </div>
       </div>
-     
     </div>
-  </div>
-  )
+  );
 }
