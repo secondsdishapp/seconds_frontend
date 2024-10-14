e8f8b1790155e55eb50f9876ddb92c66e3e8427f
AntCannon <anthonycannonier@gmail.com>
2024-10-14 12:01:15 -0400

    add CarouselCard

--- src/Components/SlidingCarousel/SlidingCarousel.jsx
+++ src/Components/SlidingCarousel/SlidingCarousel.jsx
@@ -2,6 +2,7 @@ import "./SlidingCarousel.css";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import { useState, useEffect } from "react";
 import { useSwipeable } from "react-swipeable";
+import CarouselCard from "./CarouselCard.jsx";
 
 export default function SlidingCarousel({ filteredDishSearch, locationsInRadius }) {
 
@@ -38,55 +39,12 @@ export default function SlidingCarousel({ filteredDishSearch, locationsInRadius 
       delta: 10
   });
 
-  function dishRating(rating) {
-      let arrRating = rating.split(".");
-      let firstIndex = arrRating[0];
-      let ratingIcons = [];
-
-      for (let i = 1; i <= firstIndex; i++) {
-          ratingIcons.push("full");
-      }
-
-      if  (Number(arrRating[1]) !== 0) {
-          if (Number(arrRating[1]) <= 50) {
-              ratingIcons.push("half")
-          } else if (Number(arrRating[1]) > 50) {
-              ratingIcons.push("full")
-          }
-      }
-      return ratingIcons;
-  }
-
   return (
   <div {...swipeHandlers} className="carousel-container" onClick={(e) => e.preventDefault()}>
     <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevSlide}/>
-    {filteredDishSearch.map((dish, index) => {
-      return (
-        <div key={index} className={slide === index ? "single-card" : "single-card hidden"}>
-          <div style={{display: "flex", marginLeft: "0%", alignItems: "center", width: "100%", gap: "2%"}}>
-              {dishRating(dish.avg_rating).map((elem, index) => elem === "full" ?
-                <img className="rate-icon" src="/eat.png"/>
-                : <img className="rate-icon" src="/eat-half.png"/>)
-              }
-          </div>
-          <img className="dish-image" src={dish.dish_image} onClick={(e) => e.preventDefault()}/>
-          <div className="type-icon-rating-container" >
-            <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "40px"}}>
-                {dish.vegetarian ? <img className="food-type-icon" src="/vegan.svg"/> : null} 
-                {dish.gluten_free ? <img className="food-type-icon2" src="/gluten-free.svg"/> : null}
-            </div>
-          </div>
-          <div className="rest-icon-name-container">
-            <img className="rest-icon" src="/dinner.svg" /> 
-            <p className="title-restaurant">"{dish.dish_name}"</p>
-          </div>
-          <div className="rest-icon-name-container">
-            <img className="rest-icon" src="/restaurant.svg" />
-            <p className="title-restaurant">{dish.restaurant_name}</p>  
-          </div>
-        </div>
-      )
-    })}
+    {filteredDishSearch.map((dish, index) => (
+      <CarouselCard key={index} dish={dish} index={index} slide={slide}/>
+    ))}
     <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide}/>
     <span className="indicators">
       {filteredDishSearch.map((_, index) => {
