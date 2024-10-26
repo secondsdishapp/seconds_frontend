1673bb1d5f73a0c21fa952b6b15e2c9b7bfd4c21
AntCannon <anthonycannonier@gmail.com>
2024-10-26 01:14:15 -0400

    update user service for db

--- src/Services/users.services.js
+++ src/Services/users.services.js
@@ -1,14 +1,14 @@
 const SECONDS_API = import.meta.env.VITE_API_URL;
 
 // create user from firebase
-export async function createUser(credentials) {
+export async function sendNewUserToDb(email, firebase_id) {
   try {
     const options = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(credentials)
+      body: JSON.stringify(email, firebase_id)
     };
     const res = await fetch(`${SECONDS_API}/users`, options);
     const newUser = await res.json();
